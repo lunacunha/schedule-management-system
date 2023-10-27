215f24797326340984203c7cc124a96f552dafba
Luna <lunagomescunha@gmail.com>
2023-10-27 11:09:11 +0100

    criei fil epaera por as operações que vamos fazer aos alunos - remove, add e switch (só as declarei)

---
+++ code/possible_operations.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Luna on 27/10/2023.
+//
+
+#include "possible_operations.h"
+
+/*
+ operações possiveis:
+ - remover alunos de uma uc/turma
+ - adicionar alunos a um uc/turma
+ - mudar alunos de uc/turma
+ */
+
+
+// VOID porque não vai retornar nada -> só vai mudar os valores
+
+void possible_operations::remove_students_from_uc() {
+
+}
+
+
+void possible_operations::add_students_to_uc() {
+
+}
+
+void possible_operations::switch_students_uc() {
+
+}
+
+possible_operations::possible_operations() {}
